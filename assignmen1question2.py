6bf417319f90aad3a4867994aa2de482ee1d67e8
SAURABH2006248 <dubeysaurabh98259@gmaol.com>
2025-10-07 17:07:05 +0530

    firrst python assignment

---
+++ assignmen1question2.py
@@ -0,0 +1,9 @@
+s1 = input("enter the first name :")
+s2 = input("enter the last name :")
+s = s1 + " " + s2
+print("Hello ," ,s, " ! welcome to the python programe.")
+
+# This program takes the first name and last name as input and greets the user with their full name.
+# enter the first name :saurabh
+# enter the last name :dubey
+# Hello , saurabh dubey  ! welcome to the python programe.
---
+++ assignment1ques1.py
@@ -0,0 +1,19 @@
+a = int(input("enter first number :"))
+b =  int(input("enter second number :"))
+print("Addition is:",a+b)
+print("Subtraction is:",a-b)
+print("Multiplication is:",a*b)
+print("Division is:",a/b)
+print("Floor Division is:",a//b)
+print("Modulus is:",a%b)
+print("Exponent is:",a**b)
+# This is a simple calculator program that takes two numbers as input and performs various arithmetic operations on them.#this is first question 
+# enter first number :5
+# enter second number :10
+# Addition is: 15
+# Subtraction is: -5
+# Multiplication is: 50
+# Division is: 0.5
+# Floor Division is: 0
+# Modulus is: 5
+# Exponent is: 9765625
